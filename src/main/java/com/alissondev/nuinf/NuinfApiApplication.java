7c6a17cd5287fcbe122c7dea5ee4bcc9e60a7e7f
Alisson Youssf <youssfbr@gmail.com>
2019-07-26 17:32:34 -0300

    Projeto criado

---
+++ src/main/java/com/alissondev/nuinf/NuinfApiApplication.java
@@ -0,0 +1,13 @@
+package com.alissondev.nuinf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuinfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuinfApiApplication.class, args);
+	}
+
+}

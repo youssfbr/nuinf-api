b1def52d45f4a245b69b01071721f299a83d9059
Alisson Youssf <youssfbr@gmail.com>
2019-07-26 18:33:17 -0300

    Retornando String de testes do Controlador REST

--- src/main/java/com/alissondev/nuinf/controller/PessoaController.java
+++ src/main/java/com/alissondev/nuinf/controller/PessoaController.java
@@ -1,5 +1,15 @@
 package com.alissondev.nuinf.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("nuinf/pessoas")
 public class PessoaController {
 
+	@GetMapping
+	public String findAll() {
+		return "Hello NUINF";
+	}
 }

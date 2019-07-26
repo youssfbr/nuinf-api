a5f0c6c605d5264924a47fc8e794dfc7fd0abfa2
Alisson Youssf <youssfbr@gmail.com>
2019-07-26 20:34:08 -0300

    Criando Tabela no banco em memória H2 - testes

--- src/main/java/com/alissondev/nuinf/entities/Pessoa.java
+++ src/main/java/com/alissondev/nuinf/entities/Pessoa.java
@@ -3,17 +3,29 @@ package com.alissondev.nuinf.entities;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+@Entity
 @JsonInclude(Include.NON_NULL)
 public class Pessoa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String cpf;
 	private Date dataNascimento;
 	private String email;
+	
+	@Transient
 	private List<Telefone> telefones;
 	
 	public Long getId() {
@@ -51,5 +63,28 @@ public class Pessoa {
 	}
 	public void setTelefones(List<Telefone> telefones) {
 		this.telefones = telefones;
-	}	
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}		
 }

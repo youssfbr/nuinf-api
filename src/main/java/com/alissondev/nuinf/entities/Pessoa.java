343aba98d23bca393f2f28ffd76f3e2f9447d2de
Alisson Youssf <youssfbr@gmail.com>
2019-07-26 18:50:49 -0300

    Entidades criadas

---
+++ src/main/java/com/alissondev/nuinf/entities/Pessoa.java
@@ -0,0 +1,51 @@
+package com.alissondev.nuinf.entities;
+
+import java.util.Date;
+import java.util.List;
+
+public class Pessoa {
+
+	private Long id;
+	private String nome;
+	private String cpf;
+	private Date dataNascimento;
+	private String email;
+	private List<Telefone> telefones;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}	
+}
---
+++ src/main/java/com/alissondev/nuinf/entities/Telefone.java
@@ -0,0 +1,27 @@
+package com.alissondev.nuinf.entities;
+
+public class Telefone {
+
+	private Long id;
+	private String ddd;
+	private String numero;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDdd() {
+		return ddd;
+	}
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}	
+}

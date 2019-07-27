8ecdaeb906c1bbc1b60a1aaae5e69b11da5950f7
Alisson Youssf <youssfbr@gmail.com>
2019-07-27 11:44:48 -0300

    Tratando corretamente a resposta HTTP 201

--- src/main/java/com/alissondev/nuinf/controller/PessoaController.java
+++ src/main/java/com/alissondev/nuinf/controller/PessoaController.java
@@ -1,5 +1,6 @@
 package com.alissondev.nuinf.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alissondev.nuinf.entities.Pessoa;
 import com.alissondev.nuinf.repository.PessoaRepository;
@@ -32,18 +34,23 @@ public class PessoaController {
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Pessoa> find(@PathVariable Long id) {
+	public ResponseEntity<Pessoa> findByID(@PathVariable Long id) {
 		Optional<Pessoa> pessoa = pessoas.findById(id);
+		
 		if (pessoa.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(pessoa.get());
 	}
 	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Pessoa save(@RequestBody Pessoa pessoa) {
-		return pessoas.save(pessoa);
+	@PostMapping	
+	public ResponseEntity<Void> save(@RequestBody Pessoa pessoa) {
+		pessoa = pessoas.save(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(pessoa.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build() ;
 	}
 	
 	@DeleteMapping("/{id}")

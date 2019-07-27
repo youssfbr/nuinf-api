024353ca958236ae55e832380f64a070e9efe6cd
Alisson Youssf <youssfbr@gmail.com>
2019-07-26 23:30:01 -0300

    Retornando uma pessoa pelo id

--- src/main/java/com/alissondev/nuinf/controller/PessoaController.java
+++ src/main/java/com/alissondev/nuinf/controller/PessoaController.java
@@ -1,9 +1,12 @@
 package com.alissondev.nuinf.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,15 @@ public class PessoaController {
 	@GetMapping
 	public List<Pessoa> findAll() {		
 		return pessoas.findAll();		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Pessoa> find(@PathVariable Long id) {
+		Optional<Pessoa> pessoa = pessoas.findById(id);
+		if (pessoa.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(pessoa.get());
 	}
 	
 	@PostMapping

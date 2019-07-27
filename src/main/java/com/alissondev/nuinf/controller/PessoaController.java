651e59fa08afa2206e800f182d010ab17b70d5e8
Alisson Youssf <youssfbr@gmail.com>
2019-07-27 03:06:40 -0300

    Deletando uma pessoa

--- src/main/java/com/alissondev/nuinf/controller/PessoaController.java
+++ src/main/java/com/alissondev/nuinf/controller/PessoaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,10 @@ public class PessoaController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Pessoa save(@RequestBody Pessoa pessoa) {
 		return pessoas.save(pessoa);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable Long id) {
+		pessoas.deleteById(id);
 	}
 }

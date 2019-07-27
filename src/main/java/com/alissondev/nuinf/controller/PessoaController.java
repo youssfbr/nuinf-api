bada1595070fe42f4aeda272d581f8214c9d0113
Alisson Youssf <youssfbr@gmail.com>
2019-07-26 21:47:02 -0300

    Injetando dependências e retornando todos os registros

--- src/main/java/com/alissondev/nuinf/controller/PessoaController.java
+++ src/main/java/com/alissondev/nuinf/controller/PessoaController.java
@@ -1,31 +1,24 @@
 package com.alissondev.nuinf.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alissondev.nuinf.entities.Pessoa;
+import com.alissondev.nuinf.repository.PessoaRepository;
 
 @RestController
 @RequestMapping("nuinf/pessoas")
 public class PessoaController {
 
+	@Autowired
+	private PessoaRepository pessoas;
+	
 	@GetMapping
-	public List<Pessoa> findAll() {
-		
-		Pessoa p1 = new Pessoa();
-		p1.setNome("Alisson");
-		
-		Pessoa p2 = new Pessoa();		
-		p2.setNome("Youssf");
-		
-		Pessoa[] pessoas = { p1, p2 };
-		
-		return Arrays.asList(pessoas);
-		
-		
+	public List<Pessoa> findAll() {		
+		return pessoas.findAll();		
 	}
 }

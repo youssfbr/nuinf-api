db71f5834d3da6c77acc9f4f94bf81db0972697a
Alisson Youssf <youssfbr@gmail.com>
2019-07-27 08:29:51 -0300

    Implementando o método Atualizar

--- src/main/java/com/alissondev/nuinf/controller/PessoaController.java
+++ src/main/java/com/alissondev/nuinf/controller/PessoaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -48,5 +49,11 @@ public class PessoaController {
 	@DeleteMapping("/{id}")
 	public void deleteById(@PathVariable Long id) {
 		pessoas.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public void atualizar(@RequestBody Pessoa pessoa, @PathVariable Long id) {
+		pessoa.setId(id);
+		pessoas.save(pessoa);
 	}
 }

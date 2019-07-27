e881ca4d841342abfc336009568dd3cd091e9e78
Alisson Youssf <youssfbr@gmail.com>
2019-07-26 23:52:01 -0300

    Melhorando o método save()

--- src/main/java/com/alissondev/nuinf/controller/PessoaController.java
+++ src/main/java/com/alissondev/nuinf/controller/PessoaController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alissondev.nuinf.entities.Pessoa;
@@ -22,7 +24,7 @@ public class PessoaController {
 	@Autowired
 	private PessoaRepository pessoas;
 	
-	@GetMapping
+	@GetMapping	
 	public List<Pessoa> findAll() {		
 		return pessoas.findAll();		
 	}
@@ -37,7 +39,8 @@ public class PessoaController {
 	}
 	
 	@PostMapping
-	public void save(@RequestBody Pessoa pessoa) {
-		pessoas.save(pessoa);
+	@ResponseStatus(HttpStatus.CREATED)
+	public Pessoa save(@RequestBody Pessoa pessoa) {
+		return pessoas.save(pessoa);
 	}
 }
